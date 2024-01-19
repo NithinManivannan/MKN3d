e1857e311e576d047d4f76d8bf5a0dbb396a45c8
Nithin M <nithinmds@gmail.com>
2024-01-19 18:54:57 +0530

    revalidate -> unstableRevalidate

--- src/lib/action.js
+++ src/lib/action.js
@@ -23,8 +23,8 @@ export const addPost = async (prevState,formData) => {
 
     await newPost.save();
     console.log("saved to db");
-    revalidatePath("/gifts");
-    revalidatePath("/admin");
+    res.unstable_revalidate("/gifts");
+    res.unstable_revalidate("/admin");
   } catch (err) {
     console.log(err);
     return { error: "Something went wrong!" };
