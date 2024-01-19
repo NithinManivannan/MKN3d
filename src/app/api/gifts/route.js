3598d2a661d0b56fef8747557af38c2bf34ae32b
Nithin M <nithinmds@gmail.com>
2024-01-19 21:15:45 +0530

    GET REQUEST -> PUT REQUEST

--- src/app/api/gifts/route.js
+++ src/app/api/gifts/route.js
@@ -3,7 +3,7 @@ import { Post } from "@/lib/models"
 import { connectToDb } from "@/lib/utils"
 import { NextResponse } from "next/server/"
 
-export const GET = async(request) => {
+export const PUT = async(request) => {
     try{
         connectToDb()
         const posts = await Post.find()
